a4ff21f5ad02c6b4136c3ec62747d83e627cb01d
Joe Morris <145721189+everijoe@users.noreply.github.com>
2024-11-15 10:46:03 -0600

    Adding script to sync to DEV_REPO.

---
+++ Mirror_repos_to_dev_repo.ps1
@@ -0,0 +1,34 @@
+$GitPath = "$env:USERPROFILE\OneDrive - Everi Payments, Inc\Documents\git"
+$DevRepoPath = "O:\SystemEngineering"
+
+if (Test-Path -Path $GitPath) {
+    Write-Output "Found $GitPath"
+} else {
+    throw "$GitPath not found"
+}
+
+if (Test-Path -Path $DevRepoPath) {
+    Write-Output "Found $DevRepoPath"
+} else {
+    throw "$DevRepoPath not found"
+}
+
+$ClonedRepos = @(
+    "biab-provisioning"
+    "site-controller-updates-provisioning"
+    "windows-os-patching"
+)
+
+$ClonedRepos | ForEach-Object {
+    if (Test-Path -Path $GitPath\$_) {
+        Write-Output "Found repo $_"
+        if (Select-String -Path $GitPath\$_\.git\config -Pattern '[branch "main"]') {
+            Write-Output "$_ is currently configured for the main branch"
+            Robocopy.exe $GitPath\$_\ $DevRepoPath\$_ /xd .git /mir
+        } else {
+            throw "$_ is not currently configured for the main branch"
+        }
+    }
+}
+
+Write-Output `a"Finished copying repos to DEV_REPO."
